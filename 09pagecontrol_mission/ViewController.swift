470df3001a07288b2006c861a6aaf3a12519ad5f
Haerim <34bestsun@gmail.com>
2021-03-23 01:23:46 +0900

    03_23 09_mission_pagecontrol

--- 09pagecontrol_mission/ViewController.swift
+++ 09pagecontrol_mission/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let NUM_PAGE = 10
+    @IBOutlet var lblPagenumber: UILabel!
+    @IBOutlet var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = NUM_PAGE
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.green
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        
+        lblPagenumber.text = String(pageControl.currentPage+1)
     }
-
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        lblPagenumber.text = String(pageControl.currentPage+1)
+    }
+    
 
 }
 
